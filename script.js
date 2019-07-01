13264c7c0b7e4ed818a8fa021fb64d1b808c9b98
Marina Plopeanu <plopeanu.marina@gmail.com>
2019-07-01 07:29:46 -0400

    added logic

---
+++ script.js
@@ -0,0 +1,20 @@
+
+    $(document).ready(function () {
+        $(window).scroll(function () { // check if scroll event happened
+            if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+                $(".nav").css({
+                    "background-color":"#f8f8f8", 
+                    "height":"5%"
+                });
+                $('.menu').css({
+                    "background-color": "#f8f8f8",
+                    "height": "5%"
+                });
+             // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+            } else {
+                $(".nav").css("background-color", "#b8c6c6");
+                $('.menu').css("background-color", "#b8c6c6");
+                 // if not, change it back to transparent
+            }
+        });
+    });
